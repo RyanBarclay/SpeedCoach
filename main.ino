4c698036c79e31cc5eee7588c92a5b88917cbaf8
Mark Polyakov <mark@markasoftware.com>
2020-01-11 22:25:29 -0800

    temp

--- main.ino
+++ main.ino
@@ -34,9 +34,34 @@
 // resistant but may ignore real strokes if too high.
 #define ACCEL_SAMPLES 20
 
+#define SPEED_SAMPLES 5
+
+// Cancels stroke detection if this many milliseconds have elapsed since the
+// start of a stroke.
+#define STROKE_DETECT_TIMEOUT 5000
+
+// When acceleration above the acceleration threshold is detected, we start
+// scanning for a maximum. As soon as acceleration starts to decrease, we record
+// the maximum acceleration vector. Then, 
 #define STROKE_START_ACCEL 4000
 #define STROKE_END_ACCEL 2000
-#define STROKE_END_DEG 30
+
+enum stroke_detect_state {
+	STROKE_DETECT_NULL, // in between strokes or initial
+	STROKE_DETECT_STARTED,  // The start of the stroke was detected, waiting for
+                        	// point of maximum acceleration
+	STROKE_DETECT_STROKING, // Partway through the stroke, waiting for the end of
+													// the stroke.
+};
+
+enum stroke_detect_state stroke_detect_state = STROKE_DETECT_NULL;
+
+// maximum accelerations, at the apexes of the strokes.
+int16_t x_max, y_max, z_max,
+	x_last, y_last, z_last,
+	acceleration_last,
+	state_change_millis = 0;
+
 
 LiquidCrystal_I2C lcd(LCD_I2C_ADDRESS, LCD_COLS, LCD_ROWS);
 MPU6050 accel;
@@ -46,11 +71,17 @@ SoftwareSerial ss(GPS_TX_PIN, GPS_RX_PIN);
 int16_t accel_samples[ACCEL_SAMPLES];
 int accel_sample_idx = 0;
 
-void setup() {
+int16_t speed_samples[SPEED_SAMPLES] = { 0 };
+int speed_sample_idx = 0;
+
+// Resets things like distance, speed, strokes/min, etc.
+void setup_trip() {
 	for (int i = 0; i < ACCEL_SAMPLES; i++) {
 		accel_samples[i] = 8196; // force of gravity, theoretically
 	}
+}
 
+void setup() {
 	Serial.begin(38400);
 	ss.begin(9600);
 
@@ -85,35 +116,75 @@ void setup() {
 		}
 	}
 
+	setup_trip();
+
 	lcd.setCursor(0, 0);
 	lcd.print("Ready!");
 	delay(1000); // Give the user time to read
 }
 
-int16_t x_start, y_start, z_start;
-bool in_stroke = false;
+// Return the absolute value of the acceleration and updates [xyz]-last
+// variables.
+int16_t update_acceleration() {
+	// maybe TODO: Use the interrupt to only update when new data is available.
+	accel.getAcceleration(&x_last, &y_last, &z_last);
+	accel_samples[accel_sample_idx++] = sqrt(
+		(int32_t)x_last*x_last +
+		(int32_t)y_last*y_last +
+		(int32_t)z_last*z_last);
+	int acceleration = 0;
+	for (int i = 0; i < ACCEL_SAMPLES; i++) {
+		// we don't need to worry about the integer division stuff because 
+		acceleration += accel_samples[i] / ACCEL_SAMPLES;
+	}
+	return abs(acceleration);
+}
+
+// Update GPS stuff and the display after each stroke.
+void end_of_stroke() {
+	// TODO
+}
 
 void loop() {
 	update_acceleration();
-	int16_t acceleration = average_acceleration();
-	if (!in_stroke && acceleration > STROKE_START_ACCEL) {
-		in_stroke = true;
-		accel.getAcceleration(&x_start, &y_start, &z_start);
+	int16_t acceleration = update_acceleration();
+	switch (stroke_detect_state) {
+	case STROKE_DETECT_NULL:
+	{
+		if (acceleration > STROKE_START_ACCEL) {
+			stroke_detect_state = STROKE_DETECT_STARTED;
+			state_change_millis = millis();
+		}
+		break;
 	}
-}
+	case STROKE_DETECT_STARTED:
+	{
+		if (acceleration < acceleration_last) {
+			x_max = x_last;
+			y_max = y_last;
+			z_max = z_last;
+			stroke_detect_state = STROKE_DETECT_STROKING;
+		}
+		break;
+	}
+	case STROKE_DETECT_STROKING:
+	{
+		// find the component of the acceleration that's opposite the maximum
+		// acceleration from earlier
+		if (-STROKE_END_ACCEL >
+				(int32_t)x_last * x_max +
+				(int32_t)y_last * y_max +
+				(int32_t)z_last * z_max) {
+			end_of_stroke();
+			stroke_detect_state = STROKE_DETECT_NULL;
+			// TODO: some holdoff so that a new stroke, in the wrong direction, isn't
+			// triggered immediately.
+		}
+	}
+	}
+	acceleration_last = acceleration;
 
-// Read the latest acceleration data from the sensor and add it to the moving
-// average.
-void update_acceleration() {
-	// maybe TODO: Use the interrupt to only update when new data is available.
-	int16_t x, y, z;
-	accel.getAcceleration(&x, &y, &z);
-	accel_samples[accel_sample_idx++] = sqrt((int32_t)x*x + (int32_t)y*y + (int32_t)z*z);
-}
-
-int16_t average_acceleration() {
-	int16_t result = 0;
-	for (int i = 0; i < ACCEL_SAMPLES; i++) {
-		result += accel_samples[i] / ACCEL_SAMPLES;
+	if (millis() > state_change_millis + STROKE_DETECT_TIMEOUT) {
+		stroke_detect_state = STROKE_DETECT_NULL;
 	}
 }
