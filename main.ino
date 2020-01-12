da26b7cb57c95db02ac71df93cb39ce706d13df6
ryanbarclay <mrryanbarclay@gmail.com>
2020-01-11 22:04:41 -0800

    Comments & IFDEF updates

--- main.ino
+++ main.ino
@@ -2,26 +2,31 @@
  * SpeedCoach. From nwHacks 2020 with Ryan Barclay and Mark Polyakov.
  */
 
-#include <Wire.h>
-#include <SoftwareSerial.h>
-#include <I2Cdev.h>
-#include <MPU6050.h>
-#include <TinyGPS.h>
-#include <LiquidCrystal_I2C.h>
 
-// connect to the OUT pin on touch sensor
-#define CAP_TOUCH_PIN 6
+ // Libraries
 
-// Use RX and TX pins as labeled *on the GPS*
-#define GPS_RX_PIN 8
-#define GPS_TX_PIN 9
+#include <Wire.h>								//This is for I2C (screen & accelerometer)
+#include <SoftwareSerial.h>			//This is for the GPS unit
+#include <I2Cdev.h>							//This is for I2C (reffer to above)
+#include <MPU6050.h>						//This is for (accelerometer & force unit)
+#include <TinyGPS.h>						//This is for the GPS unit
+#include <LiquidCrystal_I2C.h>	//This is for the LCD with I2C circut
 
-#define LCD_I2C_ADDRESS 0x3F
-// MPU6050 address is known by the library
 
+//Globals
+
+//Comment if not debuging
+#define DEBUG
+#define CAP_TOUCH_PIN 6				// connect to the OUT pin on touch sensor
+
+#define GPS_RX_PIN 8					// Use RX and TX pins as labeled *on the GPS*
+#define GPS_TX_PIN
+
+#define LCD_I2C_ADDRESS 0x3F	//I2C address found via https://tinyurl.com/yyz4npoz
 #define LCD_ROWS 4
 #define LCD_COLS 20
 
+// MPU6050 address is known by the library
 // accelerometer calibration
 // when setFullScaleAccessRange(1) (i.e, 1G = 8196)
 #define ACCEL_X_OFFSET -20
@@ -29,7 +34,7 @@
 #define ACCEL_Z_OFFSET 664
 
 // end pin number and stuff, begin configuration
-
+//****END OF REDONE CODE***
 // How many samples of acceleration data to use. Higher values are more noise
 // resistant but may ignore real strokes if too high.
 #define ACCEL_SAMPLES 20
@@ -38,10 +43,13 @@
 #define STROKE_END_ACCEL 2000
 #define STROKE_END_DEG 30
 
-LiquidCrystal_I2C lcd(LCD_I2C_ADDRESS, LCD_COLS, LCD_ROWS);
-MPU6050 accel;
-TinyGPS gps;
-SoftwareSerial ss(GPS_TX_PIN, GPS_RX_PIN);
+
+
+//	initialization
+LiquidCrystal_I2C lcd(LCD_I2C_ADDRESS, LCD_COLS, LCD_ROWS);	//LCD initialize
+MPU6050 accel;																							//Gyro initialize
+TinyGPS gps;																								//Gps initialize(1)
+SoftwareSerial ss(GPS_TX_PIN, GPS_RX_PIN);									//Gps start serial connection
 
 int16_t accel_samples[ACCEL_SAMPLES];
 int accel_sample_idx = 0;
@@ -55,19 +63,28 @@ void setup() {
 	ss.begin(9600);
 
 	Wire.begin();
-	
+
 	lcd.init();
 	lcd.backlight();
 
-	lcd.setCursor(0, 0);
 	lcd.print("Init: Accelerometer");
-
 	accel.initialize();
 	if (!accel.testConnection()) {
-		lcd.setCursor(0,0);
-		lcd.print("Accelerometer fail");
-		while (1) { delay(1000); }
+		#ifdef DEBUG
+			lcd.setCursor(0,0);
+			lcd.print("Accelerometer fail");
+			while (1) { delay(1000); }
+		#endif
+	}else{
+		#ifdef DEBUG
+			lcd.setCursor(0,0)
+			lcd.print("Acceleromete Work")
+			delay(1000);
+			lcd.clear()
+		#endif
 	}
+
+
 	accel.setFullScaleAccelRange(1);
 	accel.setXAccelOffset(ACCEL_X_OFFSET);
 	accel.setYAccelOffset(ACCEL_Y_OFFSET);
